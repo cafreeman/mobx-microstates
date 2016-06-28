f547ac4bb1d84ae07abc4ad2fc9d930614e43c37
Chris Freeman <cfreeman.code@gmail.com>
2016-06-28 15:23:07 -0500

    Change `value` to `values` in `List`

--- src/lib/states/List.ts
+++ src/lib/states/List.ts
@@ -2,33 +2,33 @@ import { action, observable } from 'mobx';
 import { IObservableArray } from 'mobx';
 
 export class List<T> {
-  @observable value: IObservableArray<T>;
+  @observable values: IObservableArray<T>;
 
-  constructor(value: Array<T> = []) {
-    this.value = observable(value);
+  constructor(values: Array<T> = []) {
+    this.values = observable(values);
   }
 
   @action add(item: T): void {
-    this.value.push(item);
+    this.values.push(item);
   }
 
   @action remove(item: T): boolean {
-    return this.value.remove(item);
+    return this.values.remove(item);
   }
 
   @action push(item: T, ...items: Array<T>): number {
-    return this.value.push(item, ...items);
+    return this.values.push(item, ...items);
   }
 
   @action pop(): T {
-    return this.value.pop();
+    return this.values.pop();
   }
 
   @action shift(): T {
-    return this.value.shift();
+    return this.values.shift();
   }
 
   @action unshift(item: T, ...items: Array<T>): number {
-    return this.value.unshift(item, ...items);
+    return this.values.unshift(item, ...items);
   }
 }
--- src/tests/List.spec.ts
+++ src/tests/List.spec.ts
@@ -16,32 +16,32 @@ describe('List of strings', () => {
     });
 
     it('should contain a value property, which is an array', () => {
-      assert.property(dummy, 'value', 'value property should exist');
-      assert.deepEqual(dummy.value.constructor['name'], "ObservableArray");
+      assert.property(dummy, 'values', 'value property should exist');
+      assert.deepEqual(dummy.values.constructor['name'], "ObservableArray");
     });
   });
 
   describe('add', () => {
     it('should add a value to the array', () => {
       dummy.add('456');
-      assert.lengthOf(dummy.value, 4, 'dummy.value should now be 4 items');
-      assert.deepEqual(dummy.value[3], '456', 'add should push "456" to the end of the array');
+      assert.lengthOf(dummy.values, 4, 'dummy.values should now be 4 items');
+      assert.deepEqual(dummy.values[3], '456', 'add should push "456" to the end of the array');
     });
   });
 
   describe('remove', () => {
     it('should remove the item from the array', () => {
-      let removedValue = dummy.value[1];
-      let lastValue = dummy.value[2];
+      let removedValue = dummy.values[1];
+      let lastValue = dummy.values[2];
       dummy.remove(removedValue);
-      assert.lengthOf(dummy.value, 2, 'dummy.value should now be 2 items');
-      assert.notDeepEqual(dummy.value[1], removedValue, 'value[1] should now be a different value after remove');
-      assert.deepEqual(dummy.value[1], lastValue, 'value[1] should now be equal to the last value');
+      assert.lengthOf(dummy.values, 2, 'dummy.values should now be 2 items');
+      assert.notDeepEqual(dummy.values[1], removedValue, 'value[1] should now be a different value after remove');
+      assert.deepEqual(dummy.values[1], lastValue, 'value[1] should now be equal to the last value');
     });
 
     it('should return true if the item was removed', () => {
-      let targetValue = dummy.value[1];
-      let removed = dummy.remove(dummy.value[1]);
+      let targetValue = dummy.values[1];
+      let removed = dummy.remove(dummy.values[1]);
 
       assert.isTrue(removed);
     });
@@ -49,31 +49,31 @@ describe('List of strings', () => {
     it('should leave the array unchanged and return false if the item is not found', () => {
       let removed = dummy.remove('thing');
       assert.isFalse(removed, 'removing an item that doesn\'t exist should return false');
-      assert.lengthOf(dummy.value, 3), 'the array should still be 3 items after an invalid remove';
+      assert.lengthOf(dummy.values, 3), 'the array should still be 3 items after an invalid remove';
     });
   });
 
   describe('push', () => {
     it('should mimic List.add', () => {
       dummy.push('456');
-      assert.lengthOf(dummy.value, 4, 'dummy.value should now be 4 items');
-      assert.deepEqual(dummy.value[3], '456', 'add should push "456" to the end of the array');
+      assert.lengthOf(dummy.values, 4, 'dummy.values should now be 4 items');
+      assert.deepEqual(dummy.values[3], '456', 'add should push "456" to the end of the array');
     });
 
     it('should return the new length of the list', () => {
       let pushed = dummy.push('456');
-      assert.lengthOf(dummy.value, pushed);
+      assert.lengthOf(dummy.values, pushed);
     });
   });
 
   describe('pop', () => {
     it('should pop the last value off the array', () => {
       dummy.pop();
-      assert.lengthOf(dummy.value, 2, 'pop should reduce the length of the list by 1');
+      assert.lengthOf(dummy.values, 2, 'pop should reduce the length of the list by 1');
     });
 
     it('should return the popped value', () => {
-      let lastValue = dummy.value[2];
+      let lastValue = dummy.values[2];
       let popped = dummy.pop();
       assert.deepEqual(lastValue, popped, `pop didn't return the last value of the list`);
     });
@@ -82,11 +82,11 @@ describe('List of strings', () => {
   describe('shift', () => {
     it('should remove the first element from the list', () => {
       dummy.shift();
-      assert.lengthOf(dummy.value, 2, 'shift should reduce the length of the list by 1');
+      assert.lengthOf(dummy.values, 2, 'shift should reduce the length of the list by 1');
     });
 
     it('should return the first element of the array', () => {
-      let firstValue = dummy.value[0];
+      let firstValue = dummy.values[0];
       let shifted = dummy.shift();
       assert.deepEqual(firstValue, shifted, `the return value didn't match the first value in the list`)
     });
@@ -95,13 +95,13 @@ describe('List of strings', () => {
   describe('unshift', () => {
     it('should add the element to the beginning of the array', () => {
       dummy.unshift('first');
-      assert.lengthOf(dummy.value, 4, 'unshift should increase the length of the list by 1');
-      assert.deepEqual(dummy.value[0], 'first', 'the first element of the list should match the target value');
+      assert.lengthOf(dummy.values, 4, 'unshift should increase the length of the list by 1');
+      assert.deepEqual(dummy.values[0], 'first', 'the first element of the list should match the target value');
     });
 
     it('should return the new length of the array', () => {
       let unshifted = dummy.unshift('first');
-      assert.deepEqual(unshifted, dummy.value.length, 'unshift did not return the correct length');
+      assert.deepEqual(unshifted, dummy.values.length, 'unshift did not return the correct length');
     })
   });
 });
@@ -118,7 +118,7 @@ describe('List of Boolean microstates', () => {
   describe('constructor', () => {
     it('should do stuff', () => {
       assert.instanceOf(dummy, List);
-      assert.instanceOf(dummy.value[0], Boolean);
+      assert.instanceOf(dummy.values[0], Boolean);
     });
   });
 });
